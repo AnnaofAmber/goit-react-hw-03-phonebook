0f4ab3a66cb60f40c544b2f1f29cbf1e8dd7a4f5
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-29 20:06:13 +0300

    fixing component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,14 +5,13 @@ export const ContactList = ({ contacts, onDelete }) => {
   return (
     <ul className={css.list}>
       {contacts.map(contact => (
-        <li key={contact.id}>
           <Contact
-            id={contact.id}
+          id={contact.id}
+            key={contact.id}
             name={contact.name}
             number={contact.number}
             onDelete={onDelete}
           />
-        </li>
       ))}
     </ul>
   );
